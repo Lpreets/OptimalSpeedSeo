a12eab93c97f782a63f782faf90ca608ad9ae09d
Lovepreet Zelenkov Singh <113479651+Lpreets@users.noreply.github.com>
2023-12-22 11:45:58 +0100

    Improved SEO and loading Skeleton

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -6,14 +6,14 @@ const Loading = () => {
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
     <section className="nav-paddings w-full">
-    <Skeleton className="h-[274px] w-full rounded-l" />
+    <Skeleton className="h-[274px] w-full rounded-l bg-black-200/40" />
     </section>
     <section className="mt-6 flex w-full flex-col sm:mt-20">
-      <Skeleton className="h-10 w-56"/>
+      <Skeleton className="h-10 w-56 bg-black-200/40"/>
       <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-        <Skeleton className="h-[440px] w-full sm:-w[356]"/>
-        <Skeleton className="h-[440px] w-full sm:-w[356]"/>
-        <Skeleton className="h-[440px] w-full sm:-w[356]"/>
+        <Skeleton className="h-[440px] w-full sm:-w[356] bg-black-200/40"/>
+        <Skeleton className="h-[440px] w-full sm:-w[356] bg-black-200/40"/>
+        <Skeleton className="h-[440px] w-full sm:-w[356] bg-black-200/40"/>
       </div>
     </section>
     </main>
